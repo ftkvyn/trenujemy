3ffe145383a7e7f1d1731f1a350dacd41925a6dd
ftkvyn <ftkvyn@gmail.com>
2017-11-25 19:18:03 +0100

    paths changed

--- server/api/controllers/ViewsController.js
+++ server/api/controllers/ViewsController.js
@@ -14,24 +14,19 @@ module.exports = {
 		if(req.query.returnUrl){
 			req.session.returnUrl = decodeURIComponent(req.query.returnUrl);
 		}
-		return res.view('login');	
+		return res.view('auth/login');	
 	},
 
 	activate:function(req,res){
-		var user = {
-			login : 'ftkvyn@gmail.com',
-			activationCode: '6fm0xg3ho4'
-		};
-		emailService.sendActivationMail(user);  
-		return res.view('activate', {locals : {}});	
+		return res.view('auth/activate', {locals : {}});	
 	},
 
 	recoverPassword: function(req,res){
-		return res.view('recoverPassword', {locals : {}});	
+		return res.view('auth/recoverPassword', {locals : {}});	
 	},
 
 	changePassword: function(req,res){
-		return res.view('changePassword', {locals : {code: req.query.code}});	
+		return res.view('auth/changePassword', {locals : {code: req.query.code}});	
 	},
 
 	dashboard: function(req,res){
