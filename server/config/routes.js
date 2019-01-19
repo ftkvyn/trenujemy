1414279f812f3132d07c80852c548342e9918e1e
ftkvyn <ftkvyn@gmail.com>
2019-01-19 20:29:56 +0100

    new register trainer page

--- server/config/routes.js
+++ server/config/routes.js
@@ -58,7 +58,7 @@ module.exports.routes = {
 
   '/adminLogin': 'ViewsController.adminLogin',
 
-  '/registerTrainer': 'ViewsController.registerTrainer',  
+  '/dlaTrenera': 'ViewsController.registerTrainer',  
 
   '/recoverPassword': 'ViewsController.recoverPassword',
 
